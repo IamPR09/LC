26d7405ad7b6f905f469af53e20a6b6bb9d48943
IamPR09 <prashant.ranjan91@gmail.com>
2024-04-26 13:37:57 -0400

    initial commit - Apr 25

---
+++ Notes_Apr25.py
@@ -0,0 +1,26 @@
+"""
+Binary Tree : 
+
+         1
+	   /   \
+	  2     3
+     / \   /  \
+    4   5 6    7
+ 
+Properties:
+ - Has n-1 edges, if n is the number of nodes
+ - Can at most have 2 children
+ - The top most node is called root
+ - The lowest most children are called leaves.
+ 
+There are few operations that we can perform on abinary tree:
+ - Insertion
+ - Deletion
+ - Searching
+ - Traversal
+   - DFS
+     1. In Order
+     2. Pre Order
+     3. Post Order
+  - BFS
+"""
